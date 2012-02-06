5510e40402e076b8edd7a120d2caec55965439e8
GamyHuang <hguangm2009@gmail.com>
2012-02-06 18:46:06 +0800

    adjust the directories.

---
+++ GhostGameTests/GhostGameTests.h
@@ -0,0 +1,17 @@
+//
+//  GhostGameTests.h
+//  GhostGameTests
+//
+//  Created by gamy on 12-2-2.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface GhostGameTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ GhostGameTests/GhostGameTests.m
@@ -0,0 +1,33 @@
+//
+//  GhostGameTests.m
+//  GhostGameTests
+//
+//  Created by gamy on 12-2-2.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GhostGameTests.h"
+
+
+@implementation GhostGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GhostGameTests");
+}
+
+@end
