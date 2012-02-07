e76224ead6e5f42ffc300c469b33454b00147afc
haodong <qiuhaodong2006@126.com>
2012-02-07 15:18:09 +0800

    add HelpController

---
+++ GhostGame/ViewController/HelpController.h
@@ -0,0 +1,13 @@
+//
+//  HelpController.h
+//  GhostGame
+//
+//  Created by haodong qiu on 12年2月7日.
+//  Copyright (c) 2012年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpController : UIViewController
+
+@end
