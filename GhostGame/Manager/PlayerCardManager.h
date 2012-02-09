1970fe6f79bb19b847ec564f2e02868b9efc47e1
GamyHuang <hguangm2009@gmail.com>
2012-02-09 15:49:22 +0800

    refactor files.

---
+++ GhostGame/Manager/PlayerCardManager.h
@@ -0,0 +1,26 @@
+//
+//  PlayerCardManager.h
+//  GhostGame
+//
+//  Created by  on 12-2-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "PlayerCard.h"
+
+@interface PlayerCardManager : NSObject<PlayerCardDelegate>
+{
+    NSInteger _pickIndex;
+    PlayerCard *_showingCard;
+}
+@property(nonatomic, readonly) PlayerCard *showingCard;
+@property(nonatomic, retain)NSMutableArray *playerCardList;
+
++ (PlayerCardManager *)defaultManager;
+- (PlayerCard *)playerCardAtIndex:(NSInteger)index;
+- (NSInteger)indexOfPlayerCard:(PlayerCard *)playerCard;
+- (NSInteger)playerCardCount;
+@end
+extern PlayerCardManager *GlobalGetPlayerCardManager();
